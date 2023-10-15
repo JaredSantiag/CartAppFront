d04ca7f2648f2ebd36ca8363b8540af5862eec87
Jared Santiago Garcia <jaredarturo105@gmail.com>
2023-10-14 23:04:37 -0600

    first commit

---
+++ src/CartApp.jsx
@@ -0,0 +1,74 @@
+export const CartApp = () => {
+    return (<>
+        <div className="container">
+            <h3>Cart App</h3>
+            <div className="row">
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Teclado Mecanico RGB</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text">$ 100.00</p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Samsung Smart TV 55</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text">$ 500.00</p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Audifonos Bluetooth Sony</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text">$ 70.00</p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Memeroia Corsair 8GB DDR5</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text">$ 300.00</p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Asus Nvdia RTX</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text">$ 800.00</p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-4 my-2">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">CPU Intel Core I5</h5>
+                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
+                            <p className="card-text">$ 1000.00</p>
+                            <button className="btn btn-primary">Agregar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>)
+}
