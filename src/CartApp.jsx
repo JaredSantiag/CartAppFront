3e4125a0f2479c0e966b13e18a8744d3f40515af
Jared Santiago Garcia <jaredarturo105@gmail.com>
2023-10-15 22:41:25 -0600

    Data y Service de productos

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,73 +1,63 @@
+import { useEffect } from "react";
+import { getProducts } from "./services/productService";
+import { useState } from "react";
+
 export const CartApp = () => {
+
+   const [products, setProducts] = useState([]);
+   useEffect(
+    () => {
+        setProducts(getProducts());
+    }, []);
+
     return (<>
         <div className="container">
             <h3>Cart App</h3>
             <div className="row">
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Teclado Mecanico RGB</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text">$ 100.00</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
 
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Samsung Smart TV 55</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text">$ 500.00</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
+                {products.map(prod => (
 
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Audifonos Bluetooth Sony</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text">$ 70.00</p>
-                            <button className="btn btn-primary">Agregar</button>
+                    <div className="col-4 my-2" key={prod.id}>
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{ prod.name }</h5>
+                                <p className="card-text">{ prod.description }</p>
+                                <p className="card-text">$ { prod.price }</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ))}
 
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Memeroia Corsair 8GB DDR5</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text">$ 300.00</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
+            </div>
 
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Asus Nvdia RTX</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text">$ 800.00</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-4 my-2">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">CPU Intel Core I5</h5>
-                            <p className="card-text">Teclado mecanico con luces RGB switches cherry red</p>
-                            <p className="card-text">$ 1000.00</p>
-                            <button className="btn btn-primary">Agregar</button>
-                        </div>
-                    </div>
-                </div>
+            <div className="my-4 w-50">
+                <table className="table table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Precio</th>
+                            <th>Cantidad</th>
+                            <th>Total</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>nombre</td>
+                            <td>precio</td>
+                            <td>cantidad</td>
+                            <td>total</td>
+                            <td>eliminar</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan="3" className="text-end fw-bold">Total</td>
+                            <td colSpan="2" className="text-start fw-bold">$1299.00</td>
+                        </tr>
+                        </tfoot>
+                </table>
             </div>
         </div>
     </>)
