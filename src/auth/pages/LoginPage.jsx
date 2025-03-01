5c4ef9f2f2562d818bc672fe93198500eae553b7
Jared Santiago Garcia <jaredarturo105@gmail.com>
2025-03-01 00:11:58 -0600

    STYLE: Se cambia color fondo

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Swal from "sweetalert2";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
+import '../styles/login.css';
 
 const initialLoginForm = {
     username: '',
@@ -10,7 +11,7 @@ const initialLoginForm = {
 
 export const LoginPage = () => {
 
-    const {handlerLogin} = useAuth();
+    const { handlerLogin } = useAuth();
     const navigate = useNavigate();
 
     const [LoginForm, setLoginForm] = useState(initialLoginForm);
@@ -41,42 +42,50 @@ export const LoginPage = () => {
         navigate('/register');
     }
 
-    return (
-        <div className="modal" tabIndex="-1" style={{ display: 'block' }}>
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Login page</h5>
-                    </div>
-                    <form onSubmit={onSubmit}>
-                        <div className="modal-body">
-                            <input
-                                className="form-control my-3 w-75"
-                                placeholder="Username"
-                                name="username"
-                                value={username}
-                                onChange={onInputChange} />
-
-                            <input
-                                className="form-control my-3 w-75"
-                                type="password"
-                                placeholder="Password"
-                                name="password"
-                                value={password}
-                                onChange={onInputChange} />
-
-                        </div>
-                        <div className="modal-footer d-flex justify-content-between">
-                            <button type="submit" className="btn btn-primary justify-content-end">Login</button>
-
-                            <div>
-                                Don't have an account?
-                                <button type="button" className="btn btn-link" onClick={onRegisterUser}>Register here</button>
+    return (<>
+        <div className="row principal-login">
+            <div className="col">
+                
+            </div>
+            <div className="col column-login">
+                <div className="modal modal-login" tabIndex="-1" style={{ display: 'block' }}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Login</h5>
                             </div>
+                            <form onSubmit={onSubmit}>
+                                <div className="modal-body">
+                                    <input
+                                        className="form-control my-3 w-75"
+                                        placeholder="Username"
+                                        name="username"
+                                        value={username}
+                                        onChange={onInputChange} />
+
+                                    <input
+                                        className="form-control my-3 w-75"
+                                        type="password"
+                                        placeholder="Password"
+                                        name="password"
+                                        value={password}
+                                        onChange={onInputChange} />
+
+                                </div>
+                                <div className="modal-footer d-flex justify-content-between">
+                                    <button type="submit" className="btn btn-primary justify-content-end">Login</button>
+
+                                    <div>
+                                        Don't have an account?
+                                        <button type="button" className="btn btn-link" onClick={onRegisterUser}>Register here</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    );
+
+    </>);
 }
